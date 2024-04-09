6ef0ca219312c7a69c8110c846262ac1e61066e6
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-04-09 14:08:15 -0400

    add control block operations

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ extern crate alloc;
 const RC_SINGLE: usize = 4;
 const WEAK: usize = 2;
 const CLOSED: usize = 1;
-const MAX_REFCOUNT: usize = (usize::MAX as usize) & !(RC_SINGLE - 1);
+const MAX_STRONG_COUNT: usize = isize::MAX as usize >> 2; // 61 bits for strong count
+const MAX_WEAK_COUNT: usize = isize::MAX as usize; // 63 bits for weak count
 
 use core::{
     alloc::{Allocator, Layout},
@@ -34,20 +35,72 @@ pub struct Arc<T: ?Sized, A: Allocator = Global> {
 
 impl<T: ?Sized> ArcInner<T> {
     unsafe fn release_weak<A: Allocator>(this: NonNull<Self>, alloc: A) {
+        // it is ok to use relaxed ordering since we are to do fetch_sub later on
         let old = this.as_ref().weak.load(Ordering::Relaxed);
         if old == 1 || this.as_ref().weak.fetch_sub(1, Ordering::AcqRel) == 0 {
             Box::from_raw_in(this.as_ptr(), alloc);
         }
     }
     unsafe fn release_strong<A: Allocator>(mut this: NonNull<Self>, alloc: A) {
-        let old = this.as_ref().strong.fetch_sub(RC_SINGLE, Ordering::AcqRel);
+        let old = this.as_ref().strong.fetch_sub(RC_SINGLE, Ordering::Relaxed);
         if old > RC_SINGLE + WEAK {
+            // object is still alive, return
             return;
         }
         if old & WEAK == 0 {
+            // this RC has never been weakly referenced
+            // drop it immediately
             core::ptr::drop_in_place(&mut this.as_mut().data);
             Box::from_raw_in(this.as_ptr(), alloc);
             return;
         }
+        if this
+            .as_ref()
+            .strong
+            .compare_exchange(WEAK, CLOSED, Ordering::AcqRel, Ordering::Relaxed)
+            .is_ok()
+        {
+            // Previous CAS successfully closed the object. Thus, it is our responsibility to drop the associated data.
+            core::ptr::drop_in_place(&mut this.as_mut().data);
+        }
+        // Release the implicitly held weak reference
+        Self::release_weak(this, alloc);
+    }
+    unsafe fn acquire_weak(this: NonNull<Self>) {
+        // it is ok to use relaxed ordering since we are to do compare_exchange later on
+        let old = this.as_ref().weak.load(Ordering::Relaxed);
+        if old == 0 {
+            if this
+                .as_ref()
+                .weak
+                .compare_exchange(old, 2, Ordering::AcqRel, Ordering::Relaxed)
+                .is_ok()
+            {
+                // acquire the implicit weak reference
+                this.as_ref().strong.fetch_add(WEAK, Ordering::Release);
+                return;
+            }
+        }
+        // acquire the explicit weak reference
+        let weak = this.as_ref().weak.fetch_add(1, Ordering::Relaxed);
+        if core::intrinsics::unlikely(weak > MAX_WEAK_COUNT) {
+            core::intrinsics::abort();
+        }
+    }
+    unsafe fn acquire_strong(this: NonNull<Self>) {
+        let val = this.as_ref().strong.fetch_add(RC_SINGLE, Ordering::Relaxed);
+        if core::intrinsics::unlikely((val >> 2) > MAX_STRONG_COUNT) {
+            core::intrinsics::abort();
+        }
+    }
+    unsafe fn acquire_strong_from_weak(this: NonNull<Self>) -> bool {
+        let old = this.as_ref().strong.fetch_add(RC_SINGLE, Ordering::Relaxed);
+        if old & CLOSED != 0 {
+            return false;
+        }
+        if old < RC_SINGLE {
+            Self::acquire_weak(this);
+        }
+        true
     }
 }
